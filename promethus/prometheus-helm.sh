c23f360ee195643fce50e41b19ea03e3a65e678a
jwkam <44567894+jwkam@users.noreply.github.com>
2019-07-11 13:46:53 +0900

    Delete prometheus-helm.sh

--- promethus/prometheus-helm.sh
+++
@@ -1,3 +0,0 @@
-helm install stable/prometheus --name prometheus --namespace monitoring --set server.persistentVolume.enabled=false --set alertmanager.persistentVolume.enabled=false --set alertmanager.service.type=NodePort --set server.service.type=NodePort
-kubectl get --namespace monitoring -o jsonpath="{.spec.ports[0].nodePort}" services prometheus-server
-kubectl get --namespace monitoring -o jsonpath="{.spec.ports[0].nodePort}" services prometheus-alertmanager
