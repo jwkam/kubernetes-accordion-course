d73696711e5148f4095c7d739bdf8b31e58513f1
jwkam <44567894+jwkam@users.noreply.github.com>
2019-07-11 13:47:26 +0900

    Delete adapter-helm.sh

--- promethus/adapter-helm.sh
+++
@@ -1,1 +0,0 @@
-helm install stable/prometheus-adapter --name prometheus-adapter --namespace monitoring --set prometheus.url=http://prometheus-server.monitoring.svc.cluster.local  --set prometheus.port=80
