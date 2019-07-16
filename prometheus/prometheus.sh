b1958b2a012f4f2b4b65f3fa86f15d9fd241c6cf
jwkam <44567894+jwkam@users.noreply.github.com>
2019-07-16 09:27:10 +0900

    Update prometheus.sh

--- prometheus/prometheus.sh
+++ prometheus/prometheus.sh
@@ -1,3 +1,7 @@
 helm install stable/prometheus --name prometheus --namespace monitoring --set server.persistentVolume.enabled=false --set alertmanager.persistentVolume.enabled=false --set alertmanager.service.type=NodePort --set server.service.type=NodePort
-kubectl get --namespace monitoring -o jsonpath="{.spec.ports[0].nodePort}" services prometheus-server
-kubectl get --namespace monitoring -o jsonpath="{.spec.ports[0].nodePort}" services prometheus-alertmanager
+
+export SERVER_PORT=$(kubectl get --namespace monitoring -o jsonpath="{.spec.ports[0].nodePort}" services prometheus-server)
+export ALERTMANAGER_PORT=$(kubectl get --namespace monitoring -o jsonpath="{.spec.ports[0].nodePort}" services prometheus-alertmanager)
+
+echo PrometheusServer_NodePort=$SERVER_PORT
+echo alertmanager_NodePort=$ALERTMANAGER_PORT
