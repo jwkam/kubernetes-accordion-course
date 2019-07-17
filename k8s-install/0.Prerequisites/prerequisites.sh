7a337377104b3f760483ff5b0eccf1cc6ea9370c
jwkam <jwkam@mantech.co.kr>
2019-07-17 09:40:55 +0900

    no message

--- k8s-install/0.Prerequisites/prerequisites.sh
+++ k8s-install/0.Prerequisites/prerequisites.sh
@@ -2,5 +2,6 @@ setenforce 0
 sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
 systemctl stop firewalld
 systemctl disable firewalld
+
 yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum -y install jq git net-tools wget stress
---
+++ k8s-install/2.kubernetes/4.after_init.sh
@@ -0,0 +1,8 @@
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+export KUBECONFIG=/etc/kubernetes/admin.conf
+
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+
+kubectl taint nodes --all node-role.kubernetes.io/master-
