3c1536c27f9d6c2290bf98bfcc1e0cd4ae6d69e6
jwkam <44567894+jwkam@users.noreply.github.com>
2020-06-17 15:08:57 +0900

    Update 2.helm_install.sh

--- k8s-install/3.helm/2.helm_install.sh
+++ k8s-install/3.helm/2.helm_install.sh
@@ -1,3 +1,4 @@
 kubectl create serviceaccount --namespace kube-system tiller
 kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
-helm init --service-account tiller
+#helm init --service-account tiller
+helm init --service-account tiller --override spec.selector.matchLabels.'name'='tiller',spec.selector.matchLabels.'app'='helm' --output yaml | sed 's@apiVersion: extensions/v1beta1@apiVersion: apps/v1@' | kubectl apply -f -
